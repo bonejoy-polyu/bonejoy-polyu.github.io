5de9ee29f7a79160748ab94762616a5d6a5c67f4
bonejoy-polyu <bonejoy.polyu@gmail.com>
2021-10-05 04:54:11 +0800

    bug fixes

--- assets/js/myMain.js
+++ assets/js/myMain.js
@@ -15,29 +15,38 @@ function init() {
 
 function slide(elem) {
     var sliderWrap_w = document.getElementById("paper-slider").offsetWidth;
-    var fullSlider_w = document.getElementById("paper-row").offsetWidth;
     var sliderWrap_rightPos = document.getElementById("paper-slider").getBoundingClientRect().right;
-    var sliderWrap_leftPos = document.getElementById("paper-slider").getBoundingClientRect().left;
     var fullSlider_leftPos = document.getElementById("paper-row").getBoundingClientRect().left;
     var fullSlider_rightPos = document.getElementById("paper-row").getBoundingClientRect().right;
 
     var bxCanFit = Math.floor(sliderWrap_w/(bxW+bxMargin*2));
     var slided = sliderWrap_rightPos-fullSlider_leftPos;
     var numOfSlidedBx = Math.floor(slided/(bxW+bxMargin*2));
+    var curSlidePos = parseInt(document.getElementById("paper-row").style.left, 10);
+    if (isNaN(curSlidePos)) curSlidePos = 0;
+    // edge cases
+    if (bxCanFit == 0) bxCanFit = 1;
+    if (numOfSlidedBx == 0) numOfSlidedBx = 1;
     
     var slideTo = 0;
-    if (elem.parentNode.id == "right-arrow") {
-        slideTo = -(document.getElementsByClassName("paper-block")[numOfSlidedBx].getBoundingClientRect().left-fullSlider_leftPos);
-        if ((fullSlider_rightPos-sliderWrap_rightPos) <= sliderWrap_w) slideTo = parseInt(document.getElementById("paper-row").style.left, 10)-(fullSlider_rightPos-sliderWrap_rightPos);
-    } else if (elem.parentNode.id == "left-arrow") {
+    if (elem.id == "right-arrow") {
+
+        if ((fullSlider_rightPos-sliderWrap_rightPos) <= sliderWrap_w)
+            slideTo = curSlidePos-(fullSlider_rightPos-sliderWrap_rightPos);
+        else
+            slideTo = curSlidePos-(bxCanFit*(bxW+bxMargin*2));
+
+    } else if (elem.id == "left-arrow") {
+
         if (numOfSlidedBx-bxCanFit <= bxCanFit)
             slideTo = 0;
         else
-            slideTo = parseInt(document.getElementById("paper-row").style.left, 10)+(sliderWrap_rightPos-document.getElementsByClassName("paper-block")[numOfSlidedBx-bxCanFit].getBoundingClientRect().right);
+            slideTo = curSlidePos+(bxCanFit*(bxW+bxMargin*2));
+
     }
-    console.log(numOfSlidedBx);
-    console.log(bxCanFit);
-    console.log(slideTo);
+    // console.log(numOfSlidedBx);
+    // console.log(bxCanFit);
+    // console.log(slideTo);
     document.getElementById("paper-row").style.left = slideTo+"px";
     // if slideTo > remaining slide then just slide to remaining
 }
